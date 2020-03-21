ae984ee907c33c208e0e999a872a67ce5d20a72a
Spencer Anderson <se.anderson88@gmail.com>
2020-03-21 14:05:23 -0500

    Updated code, added more functions and tests

--- app_wp.py
+++ app_wp.py
@@ -1,16 +1,25 @@
+#!/usr/local/bin/env python3 
+
 import requests
 import json
 import base64
 import os
 from datetime import datetime
 from configparser import ConfigParser
+import argparse
+
 
 def get_conf(file):
     config = ConfigParser()
     config.read(file)
-    creds = config.get('wp', 'username'), config.get('wp', 'passwd')
-    return creds
+    username = config.get('wp', 'username')
+    passwd = config.get('wp', 'passwd')
+    return username, passwd
 
+
+def format_creds(creds):
+    username, passwd = creds
+    return f'{username}:{passwd}'
 
 # URLs
 url = 'http://correando.wpengine.com'
@@ -21,27 +30,60 @@ local_url = 'http://correando.local'
 local_wapi_url = f'{local_url}/wp-json/wp/v2'
 
 
-creds = f'{username}:{passwd}'
-token = base64.b64encode(creds.encode())
+def get_creds(file):
+    return get_conf(file)
 
-header = {
-    'Authorization':'Basic ' + token.decode('utf-8'), 
-    'Content-Disposition': 'attachment; filename=desk-design.png',
+
+def fmt_creds(creds)
+    return format_creds(creds)
+
+
+def token(creds_fmt):
+    return base64.b64encode(creds_fmt.encode())
+
+
+def get_header(token):
+    # Right now only working with the basic auth and media uploading. 
+    # Will change options later. 
+    return header = {
+        'Authorization':'Basic ' + token.decode('utf-8'), 
+        'Content-Disposition': 'attachment; filename=desk-design.png',
+        }
+
+
+def wapi_base(remote=None, domain):
+    if 'Yes' or 'y' in remote:
+        return f'{domain}.wpengine.com'
+    else:
+        return f'{domain}.local'
+
+
+def is_media(remote=None, wapi_base):
+
+    if 'Yes' or 'y' in remote:
+        return f'{wapi_base}/media'
+    else:
+        return f'{wapi_base}/media'
+
+
+def media_info(media_api_path):
+
+    return media = {
+
+        'file': open(f'{media_api_path}/desk_design_01.png', 'rb'),
+        'caption': 'One of two. Second API image',
+        'date': f'{datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S")}',
+        'description': 'Image of desk API', 
+        'title': 'Desks',
+        'status': 'publish'
     }
 
-media = {
-    'file': open(f'{local_media_path}/desk_design_01.png', 'rb'),
-    'caption': 'One of two. Second API image',
-    'date': f'{datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S")}',
-    'description': 'Image of desk API', 
-    'title': 'Desks',
-    'status': 'publish'
-}
 
-media_url = f'{wapi_url}/media'
-local_media_url = f'{local_wapi_url}/media'
-post_url = f'{wapi_url}/posts'
-local_post_url = f'{local_wapi_url}/posts'
+def is_post(remote=None):
+     if 'Yes' or 'y' in remote:
+         return f'{wapi_url}/posts'
+    else:
+        return f'{local_wapi_url}/posts'
 
 
 image = requests.post(media_url, headers=header, files=media)
@@ -57,6 +99,21 @@ post = {
     'content': f'First <img src="{image_url}"',
     'status': 'publish'
 }
+
+
+def arg_parse():
+
+    parser = argparse.ArgumentParser(description='Automatically post to WP using API', prog='awapi')
+    parser.add_argument('site', type=str, help="Install we're posting to")
+    parser.add_argument('-r', dest='remote', help="Tell program to use remote location (wpengine server) or local")
+    
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = arg_parse()
+    
 
 
 wp_posts_poster = requests.post(post_url, headers=header, data=post)
--- tests/test_app_wp.py
+++ tests/test_app_wp.py
@@ -1,9 +1,29 @@
+#!/usr/local/bin/python3 
+
+
 import unittest
 import argparse
+from configparser import ConfigParser
 
-from unittest.mock import Mock
-import app_wp
+# import app_wp
 
 class TestAppWp(unittest.TestCase):
-
     
+    def setUp(self):
+        self.file = '/Users/spencer.anderson/dev/pythonshenanigans/flaskapi_crud/wp-creds'
+        config = ConfigParser()
+        username = config.get('wp'
+        passwd = config.get('wp','passwd')
+
+
+    def test_get_conf(self):
+        self.assertEqual(self.username, 'correando')
+        self.assertEqual(self.passwd, 'my56Busta*')
+
+
+    # def test_format_creds(self):
+    #     self.assertEqual('correando:my56Busta*')
+    #     self.
+
+if __name__ == '__main__':
+    unittest.main()
